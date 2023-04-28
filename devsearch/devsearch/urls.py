9729951b7ea459a665ddcbc7e1fa6de284a43812
wakabibrian <wakabibrian95@gmail.com>
2023-04-29 00:57:04 +0300

    Added a projects page

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse("Here are our projects")
+
+
+def project(request):
+    return HttpResponse("Single Project")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("projects/", projects, name="projects")
 ]
