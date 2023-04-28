7350e946ddfc20e7ce5c04be1b4d64fb8e9f8894
wakabibrian <wakabibrian95@gmail.com>
2023-04-29 01:42:04 +0300

    Added templates

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here are our projects")
+    return render(request, "projects.html")
 
 
 def project(request, pk):
-    return HttpResponse("Single Project" + ' ' + str(pk))
+    return render(request, "single-project.html")
