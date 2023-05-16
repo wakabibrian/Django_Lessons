97db412554d9fda7ff041908e4634cb3bd448485
wakabibrian <wakabibrian95@gmail.com>
2023-05-16 16:43:46 +0300

    finished example

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, "projects/projects.html")
+    page = "Projects"
+    number = 11
+    context = {"page": page, "number":number}
+    return render(request, "projects/projects.html", context)
 
 def project(request,pk):
     return render(request, "projects/single-project.html")
