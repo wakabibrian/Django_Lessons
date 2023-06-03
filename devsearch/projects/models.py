1d779b52bcdde888178244e6e98d9e0a68ac145a
wakabibrian <wakabibrian95@gmail.com>
2023-06-03 11:43:40 +0300

    Finished Many to One and Many to Many relationship

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -8,6 +8,9 @@ class Project(models.Model):
                                                           # Blank=True means that we can submit the form with the field empty
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True) # Establishes a Many to Many relationship
+    vote_total = models.IntegerField(default=0, null=True, blank=True) # default - whenever the project is first created, the value is 0
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True) # default - whenever the project is first created, the value is 0
     created = models.DateTimeField(auto_now_add=True) # Gives use the date and time stamp, 
                                                       # auto_now_add - generate the time whenever the model instance is created
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) # 16 character string of numbers and letters
@@ -15,3 +18,27 @@ class Project(models.Model):
                                               
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("up", "Up Vote"),
+        ("down", "Down Vote")
+    )
+    # owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # CASCADE - delete all the reviews if the project is deleted
+                                                                  # ForeignKey establishes a one to many relationship
+    body = models.TextField(null=True, blank=True)
+    review = models.CharField(max_length=200, choices=VOTE_TYPE) # Creates a dropdown to select among the two choices
+    created = models.DateTimeField(auto_now_add=True) 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.review
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True) 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
